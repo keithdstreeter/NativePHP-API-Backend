3274cff277a6c4720e61726e7cf322e4bbab6c6b
keithdstreeter <keith.d.streeter@gmail.com>
2026-08-06 16:54:28 -0400

    Change Notification entry

--- app/Http/Controllers/Api/V1/Notification/NotificationController.php
+++ app/Http/Controllers/Api/V1/Notification/NotificationController.php
@@ -13,10 +13,32 @@ class NotificationController extends Controller
     public function store(StoreNotificationRequest $request): JsonResponse
     {
         Log::info('Storing notification', ['request' => $request->validated()]);
-        
-        $notification = Notification::query()->create($request->validated());
 
-        Log::info('Notification created', ['notification' => $notification]);
+        // $notification = Notification::query()->create($request->validated());
+
+        // Log::info('Notification created', ['notification' => $notification]);
+
+
+        $notification = Notification::create([
+            'message' => $request->Message,
+            // 'first_name' => $request->first_name,
+            // 'last_name' => $request->last_name,
+            // 'bib' => $request->bib,
+            // 'ride_short_name' => $request->ride_short_name,
+            'date_sent' => $request->DateSent,
+        ]);
+
+                //  $newNotificationtEntry = [
+                //         'ride' => $cuesheetEntry['ride'],
+                //         'turn' => $cuesheetEntry['turn'],
+                //         'notes' => $cuesheetEntry['notes'],
+                //         'distance' => $cuesheetEntry['distance'],
+                //         'completed' => $cuesheetEntry['completed'],
+                //     ];
+
+        // Create each cuesheet entry in the database
+        //   Cuesheet::create($newCuesheetEntry); 
+        
         return response()->json($notification, 201);
     }
 }
