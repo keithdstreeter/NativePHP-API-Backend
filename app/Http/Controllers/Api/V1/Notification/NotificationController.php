c54035ce02c0ad957e4f3c8edcebe495a2b07c28
keithdstreeter <keith.d.streeter@gmail.com>
2026-08-06 12:19:00 -0400

    Add logging for incoming Notification

--- app/Http/Controllers/Api/V1/Notification/NotificationController.php
+++ app/Http/Controllers/Api/V1/Notification/NotificationController.php
@@ -6,13 +6,17 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Notification\StoreNotificationRequest;
 use App\Models\Notification;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 class NotificationController extends Controller
 {
     public function store(StoreNotificationRequest $request): JsonResponse
     {
+        Log::info('Storing notification', ['request' => $request->validated()]);
+        
         $notification = Notification::query()->create($request->validated());
 
+        Log::info('Notification created', ['notification' => $notification]);
         return response()->json($notification, 201);
     }
 }
